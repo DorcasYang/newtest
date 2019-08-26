e081ae74d636dd2019939b721c0d665510d1ee6a
DorcasYang <54299084+DorcasYang@users.noreply.github.com>
2019-08-26 16:10:33 +0800

    Add files via upload

---
+++ narssisistic/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='narssisistic',
+    version='0.1',
+    description='check if a number is narssisistic',
+    py_modules=["narssisistic"],
+    url={'https://github.com/DorcasYang/newtest'}
+    )
